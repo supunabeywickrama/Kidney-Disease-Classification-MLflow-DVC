749714bcc651ceb4f9c666af61eba21cbf4a99b4
Supun Tharaka <144601629+supunabeywickrama@users.noreply.github.com>
2026-05-06 20:15:34 +0530

    add template.py file for project structure

---
+++ template.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+
+import logging
+
+#logging string
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "cnnClassifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
