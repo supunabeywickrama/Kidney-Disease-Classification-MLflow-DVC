9975cde878729f04c45d0a3271e8549365f2496b
Supun Tharaka <144601629+supunabeywickrama@users.noreply.github.com>
2026-05-07 05:53:09 +0530

    add initial project structure and configuration files

--- template.py
+++ template.py
@@ -25,4 +25,20 @@ list_of_files = [
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
+
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} is already exists: {filepath}")
